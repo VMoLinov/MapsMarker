971967dd32ec20cd8b222ad54e5b5fd128ab0e10
VMolinov <molinov@list.ru>
2022-04-17 21:14:37 +0300

    Prefinal

--- app/src/main/java/ru/molinov/mapsmarker/MainActivity.kt
+++ app/src/main/java/ru/molinov/mapsmarker/MainActivity.kt
@@ -2,28 +2,61 @@ package ru.molinov.mapsmarker
 
 import android.Manifest
 import android.os.Bundle
-import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.yandex.mapkit.MapKitFactory
 import ru.molinov.mapsmarker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * Объект для запуска запроса разрешения. Инициализация в #onCreate()
+     * #ActivityResultContracts.RequestPermission() запрашивает разрешения,
+     * #registerForActivityResult ловит результат. Результат - isGranted может быть true или false
+     * */
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+
+    /**
+     * Переопределим функцию жизненного цикла Activity для указания ключа Yandex MapKitFactory.
+     * Используем ключ, полученный здесь - https://yandex.ru/dev/maps/mapkit
+     * В #setContentView() задаём xml ресурс из папки layout посредством view binding
+     * Далее инициализируем объект запроса разрешения и запускаем запрос разрешения
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         MapKitFactory.setApiKey("6985ce83-c00f-405e-b211-d6db564e0918") // Need to set before setContentView
         setContentView(ActivityMainBinding.inflate(layoutInflater).root)
-        supportFragmentManager.beginTransaction()
-            .add(R.id.container, MapFragment())
-            .commit()
-        val locationPermissionRequest = registerForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) {
-            if (!it) {
-                Toast.makeText(applicationContext, "Need permission", Toast.LENGTH_SHORT).show()
+        requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+                if (isGranted) {
+                    // Разрешение получено, запускаем основной фрагмент приложения
+                    // И помещаем его в container activity_main.xml
+                    supportFragmentManager.beginTransaction()
+                        .add(R.id.container, MapFragment())
+                        .commit()
+
+                } else {
+                    // Разрешение не получено, выводим диалоговое окно. AlertDialog имеет паттерн
+                    // builder - создаём экземпляр класса AlertDialog.Builder, задаём параметры,
+                    // затем создаём экземпляр диалога и показываем его пользователю
+                    AlertDialog.Builder(this)
+                        .setTitle(getString(R.string.need_permisson))
+                        .setPositiveButton(android.R.string.ok) { _, _ ->
+                            // При нажатии positive button - повторно запрашиваем разрешение
+                            requestPermission()
+                        }
+                        .create()
+                        .show()
+                }
             }
-        }
-        locationPermissionRequest.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        requestPermission()
     }
+
+    /**
+     * Запускает запрос разрешения.
+     * */
+    private fun requestPermission() =
+        requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
 }
--- app/src/main/java/ru/molinov/mapsmarker/MapFragment.kt
+++ app/src/main/java/ru/molinov/mapsmarker/MapFragment.kt
@@ -6,7 +6,6 @@ import android.content.pm.PackageManager
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
-import android.os.Parcelable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -15,56 +14,35 @@ import androidx.fragment.app.Fragment
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
-import com.yandex.mapkit.map.*
+import com.yandex.mapkit.map.CameraPosition
 import com.yandex.mapkit.map.Map
+import com.yandex.mapkit.map.MapObjectCollection
 import com.yandex.mapkit.user_location.UserLocationLayer
-import com.yandex.runtime.image.ImageProvider
-import kotlinx.parcelize.Parcelize
 import ru.molinov.mapsmarker.databinding.FragmentMapBinding
-import kotlin.random.Random
 
 class MapFragment : Fragment() {
 
+    /**
+     * Вместо findViewById мы используем view binding. _binding будет инициализирована в callback'е
+     * жизненного цикла фрагмента #onCreateView. Результатом выполнения этой функции будет привязка
+     * fragment_map.xml к MapFragment
+     * Обращаемся посредством константы binding через форсколл (!!) в тех местах, где мы уверены, что
+     * _binding != null Такой подход реализован для упрощения читабельности кода, нет нужды
+     * каждый раз обращаться к binding с сейфколлом (binding?). В callback'е жизненного цикла
+     * фрагмента #onDestroy() удаляем привязку binding к текущему представлению
+     * */
     private var _binding: FragmentMapBinding? = null
     private val binding get() = _binding!!
+
+    /**
+     * Объекты, необходимые для работы нашего функционала.
+     * map - объект карты
+     * userLocation - UI слой с иконкой месторасположения устройства
+     * locationManager -
+     * */
     private lateinit var map: Map
     private lateinit var userLocation: UserLocationLayer
     private lateinit var locationManager: com.yandex.mapkit.location.LocationManager
-    private lateinit var mapObjectCollection: MutableList<MapObjectCollection>
-    private val inputListener = object : InputListener {
-        override fun onMapTap(map: Map, point: Point) {
-            mapObjectCollection.add(map.mapObjects.addCollection())
-            val index = mapObjectCollection.lastIndex
-            val mark: PlacemarkMapObject = mapObjectCollection[index].addPlacemark(point)
-            mark.apply {
-                opacity = 0.5f
-                setIcon(ImageProvider.fromResource(requireContext(), R.drawable.mark))
-                isDraggable = true
-                addTapListener(mapObjectTapListener)
-            }
-            showBottomDialog(mapObjectCollection[index])
-        }
-
-        override fun onMapLongTap(map: Map, point: Point) {
-            map.apply {
-                mapObjects.addPlacemark(point)
-                move(CameraPosition(point, ZOOM, AZIMUTH, TILT), ANIMATION, null)
-            }
-        }
-    }
-    private val mapObjectTapListener = MapObjectTapListener { mapObject, point ->
-        mapObject.parent.remove(mapObject)
-        return@MapObjectTapListener true
-    }
-
-    private fun showBottomDialog(mapObject: MapObject) {
-        mapObject.userData = UserData(Random.nextInt(), "description")
-        val data = mapObject.userData as UserData
-        BottomSheet.newInstance(data).show(parentFragmentManager, "TAG")
-    }
-
-    @Parcelize
-    class UserData constructor(val id: Int, var description: String) : Parcelable
 
     private val locationListener = LocationListener {
         map.move(
@@ -74,14 +52,22 @@ class MapFragment : Fragment() {
         )
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         MapKitFactory.initialize(requireContext())
         userLocation =
             MapKitFactory.getInstance().createUserLocationLayer(binding.mapView.mapWindow)
-        locationManager = MapKitFactory.getInstance().createLocationManager()
+//        locationManager = MapKitFactory.getInstance().createLocationManager()
         map = binding.mapView.map
-        map.addInputListener(inputListener)
         val locationManager: LocationManager =
             (requireActivity().getSystemService(LOCATION_SERVICE)) as LocationManager
         if (ContextCompat.checkSelfPermission(
@@ -94,34 +80,6 @@ class MapFragment : Fragment() {
                 LocationManager.GPS_PROVIDER, MIN_TIME_MS, MIN_DISTANCE_M, locationListener
             )
         }
-        mapObjectCollection = mutableListOf(map.mapObjects.addCollection())
-        parentFragmentManager.setFragmentResultListener("DELETE", this) { _, bundle ->
-            val data = bundle.getParcelable<UserData>("KEY")
-            for (mapObject in mapObjectCollection) {
-                if (mapObject.userData == data) {
-                    mapObject.clear()
-                }
-            }
-        }
-        parentFragmentManager.setFragmentResultListener("KEY", this) { _, bundle ->
-            val data = bundle.getParcelable<UserData>("KEY")
-            for (mapObject in mapObjectCollection) {
-                mapObject.userData.apply {
-                    if (this is UserData && this == data) {
-                        mapObject.userData = data
-                    }
-                }
-            }
-        }
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentMapBinding.inflate(inflater, container, false)
-        return binding.root
     }
 
     override fun onStart() {
@@ -132,8 +90,8 @@ class MapFragment : Fragment() {
 
     override fun onStop() {
         super.onStop()
-        binding.mapView.onStop()
-        MapKitFactory.getInstance().onStop()
+//        binding.mapView.onStop()
+//        MapKitFactory.getInstance().onStop()
     }
 
     override fun onDestroy() {
